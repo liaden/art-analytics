98de837d9e4a9414fb13bebcce7efe7ea75e43cc
Joel Johnson <johnson.joel.b@gmail.com>
2016-09-10 22:25:58 +0000

    Basic layout and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navbar_activity_class(nav_item)
+    if controller.class.name.downcase.match(nav_item.downcase)
+      'active'
+    else
+      ''
+    end
+  end
 end
---
+++ spec/controllers/control_panel_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe ControlPanelController do
+  render_views
+
+  context '#index' do
+    it 'renders' do
+      get :index
+      expect(response.status).to eq 200
+    end
+  end
+end
