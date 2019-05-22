130d9e8c7f63dece721827022870bb908ff332f3
Joel <johnson.joel.b@gmail.com>
2019-05-22 09:25:48 -0500

    Update bootstrap v4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@
 module ApplicationHelper
   def navbar_activity_class(nav_item)
     if controller.class.name.downcase.match?(nav_item.downcase)
-      'active'
+      'nav-item active'
     else
-      ''
+      'nav-item'
     end
   end
 end
