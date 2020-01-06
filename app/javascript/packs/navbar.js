798841f7921d99e4a78b5733037111f070361eb4
Joel <johnson.joel.b@gmail.com>
2020-01-06 07:41:56 -0600

    Handle tabbed content with sticky footer content
    Noticed this on tag table UI, where the footer is sticky, and it was
    only picking up the first tabs footer and adjusting it.
    
    This still won't handle sticky with `bottom` other than 0 which I don't
    have a need for yet.

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,16 +1,15 @@
 export function init_subnav() {
   $('#subnavbar').on('hidden.bs.collapse', function() {
-    var sticky_item = document.querySelector('.sticky-bottom');
-    if (sticky_item != undefined) {
+    document.querySelectorAll('.sticky-bottom').forEach(function(sticky_item) {
       sticky_item.classList.remove('handle-subnav')
-    }
+
+    });
   });
 
   $('#subnavbar').on('shown.bs.collapse', function() {
-    var sticky_item = document.querySelector('.sticky-bottom');
-    if (sticky_item != undefined) {
+    document.querySelectorAll('.sticky-bottom').forEach(function(sticky_item) {
       sticky_item.classList.add('handle-subnav')
-    }
+    });
   });
 }
 
